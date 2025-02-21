7457ef3fd35c64e8ff3de6f46b486f7c1ee8eced
Moataz0000 <moatazfawzy211@gmail.com>
2025-02-21 22:32:03 +0200

    Upgraded serialziers

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import prodcut_list,product_detail
+from .views import (
+    prodcut_list,
+    product_detail,
+    order_list,
+)
 
 
 app_name = 'products'
 
 urlpatterns = [
     path('products/', prodcut_list, name='product'),
-    path('products/<int:pk>/', product_detail, name='product_detail')
+    path('products/<int:pk>/', product_detail, name='product_detail'),
+    path('orders/', order_list, name='order_list')
 
 ]
