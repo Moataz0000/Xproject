d1020ff5528d06ca010abdc82ef0bda9b50eff01
Moataz0000 <moatazfawzy211@gmail.com>
2025-02-22 12:48:36 +0200

    added serializer subclass & make some aggregation

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from .views import (
     prodcut_list,
     product_detail,
     order_list,
+    product_info,
 )
 
 
@@ -11,6 +12,7 @@ app_name = 'products'
 urlpatterns = [
     path('products/', prodcut_list, name='product'),
     path('products/<int:pk>/', product_detail, name='product_detail'),
-    path('orders/', order_list, name='order_list')
+    path('orders/', order_list, name='order_list'),
+    path('product-info/', product_info, name='product_info')
 
 ]
